17cf20052aa69ca3c8e0fadf82f5bcf8abb0f60e
unknown <ingenieriacortes17@gmail.com>
2020-11-27 14:58:12 -0500

    BD

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-const { JUEGO_APP_MONGODB_HOST, JUEGO_APP_MONGODB_DATABASE } = process.env
+const { FORMULARIO_MONGODB_HOST, FORMULARIO_MONGODB_DATABASE } = process.env
 
-const MONGODB_URI = `mongodb+srv://${JUEGO_APP_MONGODB_HOST}/${JUEGO_APP_MONGODB_DATABASE}`
+const MONGODB_URI = `mongodb+srv://${FORMULARIO_MONGODB_HOST}/${FORMULARIO_MONGODB_DATABASE}`
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
